16b5b83538a26b6e28ae92960ceb3c7eb02df6e3
karuana <emilykaruanamwangi@gmail.com>
2025-05-21 05:42:38 +0300

    Submit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 5, 'M', 'brown', 'friendly', 1),
+('Rowdy', 3, 'M', 'black', 'playful', 1),
+('Tabitha', 7, 'F', 'gray', 'calm', 1),
+('Sergeant Brown', 8, 'M', 'brown', 'strict', 1),
+('Melissa', 2, 'F', 'white', 'shy', 1),
+('Grinch', 6, 'M', 'green', 'grumpy', 0),
+('Wendy', 4, 'F', 'brown', 'happy', 1),
+(NULL, NULL, NULL, NULL, NULL, NULL);
